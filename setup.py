a03551e6bbc8113c3e3013e5938888ceea856d79
Stephen Mitchell <scum@mac.com>
2015-08-24 17:17:15 -0400

    Setup.py file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pubhub',
+    version='0.1',
+    description='Python Socket interaction made easy.',
+    author='Stephen Mitchell',
+    url='https://github.com/scuml/pubhub',
+    packages=['pubhub']
+)
